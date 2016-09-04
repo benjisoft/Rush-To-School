617d982ea9ecae47aeaf54df8f5db7d2cf7cad5a
Benji <benji@benjicraft.info>
2016-09-04 19:29:02 +0100

    Added disappear scripts

---
+++ Assets/Scripts/Disapear on hit.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(Hit : Collider)
+    {
+        if(Hit.name == "Player") // you can compare tags instead: if (Hit.tag = "Player")
+        {
+            GetComponent.<Renderer>().enabled = false; // renderer alone is the trigger renderer
+        }
+    }
