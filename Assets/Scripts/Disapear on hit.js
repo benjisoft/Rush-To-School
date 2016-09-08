f8bf9b6ae310baafa37e86a6ca1361ac97d0bfa4
Benji <benji@benjicraft.info>
2016-09-08 21:21:39 +0100

    Fixed disappearing issue
    Fixed the issue where the backpack would not disappear (#3)

--- Assets/Scripts/Disapear on hit.js
+++ Assets/Scripts/Disapear on hit.js
@@ -12,6 +12,6 @@ function OnTriggerEnter(Hit : Collider)
     {
         if(Hit.name == "Player") // you can compare tags instead: if (Hit.tag = "Player")
         {
-            GetComponent.<Renderer>().enabled = false; // renderer alone is the trigger renderer
+            gameObject.SetActive (false); // renderer alone is the trigger renderer
         }
     }
